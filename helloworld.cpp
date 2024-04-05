3eb82a07de8a1239513b5143cd6ce86c9f5e246c
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2024-04-05 22:43:14 +0200

    add i var

--- helloworld.cpp
+++ helloworld.cpp
@@ -8,8 +8,10 @@ int main()
 {
     vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
     
+    int i = 0;
     for (const string &word : msg)
     {
+        ++i;
         cout << word << " ";
     }
     cout << endl;
